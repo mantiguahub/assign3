4fd1bd808c29dafd61677217203417b099fc86e7
mantiguahub <mantigua@montgomerycollege.edu>
2025-07-07 22:07:41 -0400

    Create pfdecrypt.java

---
+++ pfdecrypt.java
@@ -0,0 +1,8 @@
+//name:michael antigua
+//cmsc 203
+Same as encryption but:
+    If same row: replace with chars to left (wrap around)
+    If same column: replace with chars above (wrap around)
+    Else: replace with chars in same row, other’s column
+Build result string
+Return result
